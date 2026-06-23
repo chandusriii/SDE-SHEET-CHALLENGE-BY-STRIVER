514ffd4b1cb9f682e73181e835cdd7bf0490eacb
Chandu  sri <chandusri203@gmail.com>
2026-06-23 08:12:32 +0530

    Add kthLargestElement function using min-heap
    Implemented the kthLargestElement method to find the k-th largest element in an array using a min-heap.

---
+++ Day-23/K-th Largest Element in an Array.py
@@ -0,0 +1,13 @@
+import heapq
+
+class Solution:
+    def kthLargestElement(self, nums, k):
+        min_heap = []
+
+        for num in nums:
+            heapq.heappush(min_heap, num)
+
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+
+        return min_heap[0]
