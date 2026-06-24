b94367d288c4413fd0b481021115e261017eefd1
Chandu  sri <chandusri203@gmail.com>
2026-06-24 07:12:33 +0530

    Add ArrayQueue class implementation
    Implemented a queue using an array with push, pop, peek, and isEmpty methods.

--- Day_24_Stack_And_Queue/02_Implement_Queue_Using_Arrays.py
+++ Day_24_Stack_And_Queue/02_Implement_Queue_Using_Arrays.py
@@ -1,1 +1,37 @@
+class ArrayQueue:
 
+    def __init__(self):
+        self.queue = []
+        self.front = 0
+
+    def push(self, x):
+        self.queue.append(x)
+
+    def pop(self):
+        if self.isEmpty():
+            return -1
+
+        val = self.queue[self.front]
+        self.front += 1
+        return val
+
+    def peek(self):
+        if self.isEmpty():
+            return -1
+
+        return self.queue[self.front]
+
+    def isEmpty(self):
+        return self.front >= len(self.queue)
+
+
+# Example Usage
+if __name__ == "__main__":
+    queue = ArrayQueue()
+
+    queue.push(5)
+    queue.push(10)
+
+    print("Peek:", queue.peek())        # 5
+    print("Pop:", queue.pop())          # 5
+    print("Is Empty:", queue.isEmpty()) # False
